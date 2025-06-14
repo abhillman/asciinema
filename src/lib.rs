8f83d09fe698848134de7a6056408086f89c6694
Aryeh Hillman <1097794+abhillman@users.noreply.github.com>
2025-06-14 12:53:32 -0700

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+
+pub mod cli;
+pub mod cmd;
+pub mod api;
+pub mod config;
+pub mod asciicast;
+
+pub mod alis;
+pub mod encoder;
+pub mod fd;
+pub mod file_writer;
+pub mod forwarder;
+pub mod hash;
+pub mod html;
+pub mod leb128;
+pub mod locale;
+pub mod notifier;
+pub mod player;
+pub mod pty;
+pub mod server;
+pub mod session;
+pub mod status;
+pub mod stream;
+pub mod tty;
+pub mod util;
